0c88995cd75a80e51e29342cc35a4fb5ad253c12
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2021-12-30 10:45:41 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,18 +4,22 @@ pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
 let count = 0
+let front = false
+let back = false
+let left = false
+let right = false
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P15) == 0) {
-        radio.sendValue("F", 0)
+        front = true
         count += 1
     } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
-        radio.sendValue("B", 0)
+        back = true
         count += 1
     } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
-        radio.sendValue("L", 0)
+        left = true
         count += 1
     } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
-        radio.sendValue("R", 0)
+        right = true
         count += 1
     }
     led.plotBarGraph(
@@ -24,17 +28,18 @@ basic.forever(function () {
     )
 })
 basic.forever(function () {
-    if (true) {
+    if (front == true) {
         radio.sendValue("F", 0)
-        count += 1
-    } else if (false) {
+        front = false
+    } else if (back == true) {
         radio.sendValue("B", 0)
-        count += 1
-    } else if (false) {
+        back = false
+    } else if (left == true) {
         radio.sendValue("L", 0)
-        count += 1
-    } else if (false) {
+        left = false
+    } else if (right == true) {
         radio.sendValue("R", 0)
-        count += 1
+        right = false
     }
+    basic.pause(300)
 })
